e0c1e2b7dea7bab7fe22fd8d4578d3608faa35ab
ashank2603 <ashank.priyadarshi@gmail.com>
2023-06-05 21:01:57 +0530

    Search functionality added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,19 +5,24 @@ import SupabaseProvider from '@/providers/SupabaseProvider'
 import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
 import ToasterProvider from '@/providers/ToasterProvider'
+import getSongsByUserId from '@/actions/getSongsByUserId'
 
 const font = Figtree({ subsets: ['latin'] })
+
+export const revalidate = 0;
 
 export const metadata = {
   title: 'Shadow Music App',
   description: 'Your on the go music player',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const userSongs = await getSongsByUserId();
+
   return (
     <html lang="en">
       <body className={font.className}>
@@ -25,7 +30,7 @@ export default function RootLayout({
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
-            <Sidebar>
+            <Sidebar songs={userSongs} >
               {children}
             </Sidebar>
           </UserProvider>
