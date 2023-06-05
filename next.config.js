27494bfaeef5b4749df0ace715dd72a9cd44bb18
ashank2603 <ashank.priyadarshi@gmail.com>
2023-06-05 20:04:51 +0530

    Fetch songs action created

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "cffvjstyruotnszpxapu.supabase.co",
+        ]
+    }
+}
 
 module.exports = nextConfig
